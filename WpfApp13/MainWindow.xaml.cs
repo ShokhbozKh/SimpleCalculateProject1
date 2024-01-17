1da4b0a0a2e53ae73263079478cffa43247c2622
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-17 21:06:15 +0500

    Add project files.

---
+++ WpfApp13/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // uzgaruvchilar
+            string operation = ComboBoxOperation.Text;
+            double number1 = Convert.ToDouble(txtBoxInput1.Text);
+            double number2 = double.Parse(txtBoxInput2.Text);
+            double result = 0;
+
+            // calculator hisoblash
+            if (operation == "+")
+            {
+                result = number1 + number2;
+            }
+            else if (operation == "-")
+            {
+                result = number1 - number2;
+            }
+            else if (operation == "*")
+            {
+                result = number1 * number2;
+            }
+            else if (operation == "/")
+            {
+                if (number2 != 0)
+                {
+                    result = number1 / number2;
+                }
+                else
+                {
+                    MessageBox.Show("0 ga bo'lish mumkin emas", "Xatolik", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+            // natija
+            txtBlockResult.Text = result.ToString();
+        }
+    }
+}
